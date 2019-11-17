1d2d0e3fa15d11ec294f273eeef5ef5c9b550d3f
André Almeida <almeida.andref@gmail.com>
2019-11-17 20:41:50 +0100

    adding expenses listing

--- src/shared/services/expenses.js
+++ src/shared/services/expenses.js
@@ -7,7 +7,8 @@ import httpClientFactory from '@/shared/services/http/http-client'
 const fetchExpenses = async ({ commit, state }, { tripId }) => {
   commit(mutationTypes.TOGGLE_LOADING, { loading: true })
 
-  const query = `{ expenses(trip_id: ${tripId}) { id description price }}`
+  const fields = 'id description price date category { id name } source { id name } place { id name } currency { id symbol }'
+  const query = `{ expenses(trip_id: ${tripId}) { ${fields} }}`
   const { expenses } = await httpClientFactory(state).fetchGraphql(query)
   commit(mutationTypes.SET_EXPENSES, { expenses })
 
--- src/views/expenses/Expenses.vue
+++ src/views/expenses/Expenses.vue
@@ -1,7 +1,30 @@
 <template>
   <main>
     <h1>Expenses!</h1>
-    {{ expenses }}
+    <table class="table table-striped table-hover table-scroll">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Date</th>
+          <th>Price</th>
+          <th>Place</th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Source</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="exp in expenses" v-bind:key="`exp-${exp.id}`">
+          <td>{{ exp.id }}</td>
+          <td>{{ exp.date }}</td>
+          <td>{{ exp.price }} {{ exp.currency.symbol }}</td>
+          <td>{{ exp.place.name }}</td>
+          <td>{{ exp.description }}</td>
+          <td>{{ exp.category.name }}</td>
+          <td>{{ exp.source.name }}</td>
+        </tr>
+      </tbody>
+    </table>
   </main>
 </template>
 
